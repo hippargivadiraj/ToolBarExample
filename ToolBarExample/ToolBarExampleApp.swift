410ea0e288c429e9c7db3ca2c398eeb5d456c338
Vadiraj Hippargi <Vadi@leadconsultant.com>
2020-12-19 13:00:53 -0800

    Initial Commit

---
+++ ToolBarExample/ToolBarExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToolBarExampleApp.swift
+//  ToolBarExample
+//
+//  Created by Vadiraj Hippargi on 12/19/20.
+//
+
+import SwiftUI
+
+@main
+struct ToolBarExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
